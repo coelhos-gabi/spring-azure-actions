2eff1414fada4887871fec8b1a8828d2ff05fb32
coelhos-gabi <coelhos.gabi@gmail.com>
2025-02-01 17:48:11 -0300

    Ajustes

--- src/main/java/cursos/br/com/example/controller/CursoController.java
+++ src/main/java/cursos/br/com/example/controller/CursoController.java
@@ -2,8 +2,6 @@ package cursos.br.com.example.controller;
 
 import cursos.br.com.example.dto.CursoDTO;
 import cursos.br.com.example.service.CursoService;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,7 +20,6 @@ import static org.springframework.http.HttpStatus.OK;
 
 @RestController
 @RequestMapping("/cursos")
-@Tag(name= "Cursos")
 public class CursoController {
 
     private CursoService service;
@@ -37,32 +34,27 @@ public class CursoController {
     }
 
     @GetMapping
-    @Operation(summary = "Listagem de cursos")
     public ResponseEntity<List<CursoDTO>> listarCursos(){
         return ResponseEntity.ok().body(service.buscarTodos());
     }
 
     @GetMapping("/{id}")
-    @Operation(summary = "Busca por um curso")
     public ResponseEntity<CursoDTO> buscarPor(@PathVariable("id") int id){
         return new ResponseEntity<>(service.buscarPor(id), OK);
     }
 
     @PostMapping
-    @Operation(summary = "Cadastra um curso")
     public ResponseEntity<CursoDTO> cadastrar(@RequestBody CursoDTO cursoDTO){
         return new ResponseEntity<>(service.cadastrar(cursoDTO), CREATED);
     }
 
     @PutMapping("/{id}")
-    @Operation(summary = "Atuliza informações do curso")
     public ResponseEntity<CursoDTO> atualizar(@PathVariable("id") int id,
                                               @RequestBody CursoDTO cursoDTO){
         return new ResponseEntity<>(service.atualizar(id, cursoDTO), OK);
     }
 
     @DeleteMapping("/{id}")
-    @Operation(summary = "Exclui um curso")
     public ResponseEntity<Void> excluir(@PathVariable("id") int id){
         service.excluir(id);
         return new ResponseEntity<>(NO_CONTENT);
